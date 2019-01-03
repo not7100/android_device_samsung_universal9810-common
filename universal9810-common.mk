2b34aad0c2642a0a07ad457b5e72b7effb54ec1f
ivanmeler <i_ivan@windowslive.com>
2019-01-03 13:16:48 +0000

    Remove 8.0 -> 8.1 hacks
    Change-Id: Ib0a12219c46d957153244cc36c0360541de52a47

--- universal9810-common.mk
+++ universal9810-common.mk
@@ -41,9 +41,6 @@ PRODUCT_PACKAGES += \
 
 # HIDL
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/compatibility_matrix.xml:system/compatibility_matrix.xml
-
-PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/vndk-compat/vndk-compat.rc:system/etc/init/vndk-compat.rc
 
 # Audio
